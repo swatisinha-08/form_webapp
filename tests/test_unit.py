ab2a0e8870a2067a23737cab88ef18dfa1598eb6
SWATI SINHA <108465361+swatisinha-08@users.noreply.github.com>
2025-04-28 19:56:41 +0530

    Delete tests/test_unit.py

--- tests/test_unit.py
+++
@@ -1,7 +0,0 @@
-from app import app
-
-def test_homepage():
-    tester = app.test_client()
-    response = tester.get('/')
-    assert response.status_code == 200
-    assert b'Contact Form' in response.data
